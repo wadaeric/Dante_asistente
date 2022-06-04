156b05b946b72751ea5f52e17b3a942a7bbe14c6
wadaeric <103532419+wadaeric@users.noreply.github.com>
2022-06-04 20:22:59 +0200

    carpeta chistes v1
    Esta carpeta es la que contiene los chistes e iremos aumentando el numero de ellos

---
+++ bromas/chistes.py
@@ -0,0 +1,19 @@
+CHISTES = {
+    1: " Solo hay tres tipos de personas, los que saben contar y los que no",
+    
+    2: """ 
+        Señor, en la Tierra han descubierto el código del Genoma Humano. Y en eso que Dios le contesta. Malditos Hackers, Voy a tener que cambiar la contraseña de la uifi 
+    """,
+
+    3:"""
+        Qué es un Terapeuta. 1024 Gigapeutas
+    """,
+
+    4: """
+        Yo no te quiero por tu hardware, sino por tu software
+    """,
+
+    5: """ 
+        Que le dice un gif a un jpg. Anímate hombre
+    """
+}
