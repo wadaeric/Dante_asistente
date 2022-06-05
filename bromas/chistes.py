646c777b2e65eea1e267ef6e8bd5291498e2e4f1
wadaeric <103532419+wadaeric@users.noreply.github.com>
2022-06-05 07:46:10 +0200

    Chistes v2
    hemos añadido varios chistes mas

--- bromas/chistes.py
+++ bromas/chistes.py
@@ -1,19 +1,23 @@
 CHISTES = {
     1: " Solo hay tres tipos de personas, los que saben contar y los que no",
     
-    2: """ 
-        Señor, en la Tierra han descubierto el código del Genoma Humano. Y en eso que Dios le contesta. Malditos Hackers, Voy a tener que cambiar la contraseña de la uifi 
-    """,
+    2: " Señor, en la Tierra han descubierto el código del Genoma Humano. Y en eso que Dios le contesta. Malditos Hackers, Voy a tener que cambiar la contraseña de la uifi",
 
-    3:"""
-        Qué es un Terapeuta. 1024 Gigapeutas
-    """,
+    3: " Qué es un Terapeuta. 1024 Gigapeutas",
 
-    4: """
-        Yo no te quiero por tu hardware, sino por tu software
-    """,
+    4: " Yo no te quiero por tu hardware, sino por tu software",
 
-    5: """ 
-        Que le dice un gif a un jpg. Anímate hombre
-    """
+    5: " Internet es el único que se cae y nadie se ríe",
+    
+    6: " Qué le menciona una IP a otra. Qué tramas",
+    
+    7: "Cuántos programadores hacen falta para cambiar una bombilla. Ninguno, porque es un problema hardware.",
+    
+    8: "Se abre el telón, aparece un informático y dice. Qué habéis tocado que no se cierra el telón",
+    
+    9: "Cuál es la diferencia entre batman y Bill Gates. Que cuando batman luchó contra el pingüino ganó",
+    
+    10: "Dios es real. A no ser que lo declares como integer",
+    
+    11: "Que le dice un gif a un jpg. Anímate hombre"
 }
